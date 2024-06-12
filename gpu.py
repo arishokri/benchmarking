ed5abcb8341a6c8fbb81f78e0bf62435a2b9d184
Ari Shokri <arashsh0@live.com>
2024-06-11 22:38:14 -0500

    removes requirements.txt and adds multi-thread support for gpu.pu

--- gpu.py
+++ gpu.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 import time
 
+# Adjust the following value to match your CPU's number of cores.
+tf.config.threading.set_inter_op_parallelism_threads(16)
+
 # Define the matrices to be multiplied
 matrix1 = tf.random.normal([20000, 20000])
 matrix2 = tf.random.normal([20000, 20000])
